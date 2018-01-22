47fe7b0ae3555416d6cb8ce2a7f5047c7012a95d
sowcod <771434+sowcod@users.noreply.github.com>
2018-01-22 17:07:32 +0900

    use io.BytesIO

--- binary_viewer.py
+++ binary_viewer.py
@@ -1,24 +1,28 @@
 #-*- coding: utf-8 -*-
 
+import io
+
 def print_binary(bindata) :
+    if isinstance(bindata, io.BufferedIOBase) :
+        stream = bindata
+    else:
+        stream = io.BytesIO(bindata)
+
     # print header
     print('         ' + ' '.join(map(lambda x:'{:02X}'.format(x), range(16))))
 
-    # split per 16 bytes
-    maxlen = len(bindata)
-    count = 0
-    maxcount = int(maxlen / 16)
+    # read per 16 bytes
+    position = 0
     while True :
-        position = count * 16
         position_str = '{:08X}'.format(position)
 
-        current = bindata[count*16: count*16 + 16]
+        current = stream.read(16)
+        if len(current) == 0 : break
+
         hexprint = ' '.join([''.join(d) for d in zip(*[iter(bytes.hex(current))]*2)])
         hexprint += '   ' * (16 - len(current))
         print(f'{position_str} {hexprint} {current}'.format())
-        count += 1
-        if count > maxcount:
-            break
+        position += 16
 
 if __name__ == '__main__' :
     print_binary(b'''0123456789ABCDEF0123456789ABCD''')
